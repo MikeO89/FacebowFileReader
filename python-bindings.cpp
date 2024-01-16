878b18853f00df70531599e9f93adcbd8fef15b2
Patrik Huber <p.huber@mimetrik.co.uk>
2024-01-16 17:43:50 +0000

    Added Python bindings and setup.py with local vcpkg
    - Increased minimum cmake version to 3.18.2, which the pybind11 docs recommend for CMake's new FindPython modules
    - setup.py is based on the latest version of it in the pybind11 repo. I only added a few lines at the end that clone and bootstrap vcpkg at the end, and `-B build` to get cmake to pick up vcpkg.json. It works on Windows.
    - Used old pybind11_opencv.hpp from eos - the latest version, before I deleted it from the repo (2016-ish)

---
+++ python-bindings.cpp
@@ -0,0 +1,23 @@
+#include <filesystem>
+
+#include "pybind11/pybind11.h"
+#include "pybind11/stl.h" // for std::map
+#include "pybind11/stl/filesystem.h"
+
+#include "pybind11_opencv.hpp"
+#include "FacebowFileReader.hpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(FacebowFileReader, m)
+{
+    m.doc() = "Facebow MFBA file reader Python bindings";
+
+    py::class_<mimetrik::FacebowFileReader>(m, "FacebowFileReader")
+        .def(py::init<const std::filesystem::path&>(),
+             "Construct a FacebowFileReader object for the given MFBA file.")
+        .def("get_image_count", &mimetrik::FacebowFileReader::get_image_count,
+             "Returns the number of images in the MFBA file.")
+        .def("get_image", &mimetrik::FacebowFileReader::get_image, "Doc")
+        .def("get_metadata", &mimetrik::FacebowFileReader::get_metadata, "Doc");
+}
